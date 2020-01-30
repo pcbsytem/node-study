da5883065bc9bd7bf2422db5bf58ee73b1418df0
patrick-barcelos <patrick.barcelos@orgsystem.com.br>
2020-01-30 13:35:08 -0300

    Include express

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
+const express = require('express');
+const app = express();
 const http = require('http');
 
-const servidor = http.createServer((req, res) => {
-
-  let html = '';
-  if (req.url === '/') {
-    html = `
-      <html>
-        <head>  
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1>Home</h1>
-        </body>
-      </html>        
-      `;
-  } else if (req.url === '/livros') {
-    html = `
-      <html>
-        <head>  
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1>Livros</h1>
-        </body>
-      </html>        
-    `;
-  }
-  res.end(html);
+app.listen(3001, () => {
+  console.log('Servidor roandando na porta 3000');
 });
 
-servidor.listen(3000);
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>  
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Churi</h1>
+      </body>
+    </html> 
+  `
+  );
+})
+
+// const servidor = http.createServer((req, res) => {
+
+//   let html = '';
+//   if (req.url === '/') {
+//     html = `
+//       <html>
+//         <head>  
+//           <meta charset="utf-8">
+//         </head>
+//         <body>
+//           <h1>Home</h1>
+//         </body>
+//       </html>        
+//       `;
+//   } else if (req.url === '/livros') {
+//     html = `
+//       <html>
+//         <head>  
+//           <meta charset="utf-8">
+//         </head>
+//         <body>
+//           <h1>Livros</h1>
+//         </body>
+//       </html>        
+//     `;
+//   }
+//   res.end(html);
+// });
+
+// servidor.listen(3000);
