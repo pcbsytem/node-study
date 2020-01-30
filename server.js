0b62789914a775c1743d68e0127de8869e66b4d8
pcbsytem <pcbsytem@hotmail.com>
2020-01-30 07:36:26 -0300

    Add package.json and gitignore

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ const servidor = http.createServer((req, res) => {
           <meta charset="utf-8">
         </head>
         <body>
-          <h1>Hello word</h1>
+          <h1>Home</h1>
         </body>
       </html>        
       `;
+  } else if (req.url === '/livros') {
+    html = `
+      <html>
+        <head>  
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1>Livros</h1>
+        </body>
+      </html>        
+    `;
   }
   res.end(html);
 });
