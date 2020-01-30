f0c3ca006f9d1e3adce8ff35747b7d27e02c4026
pcbsytem <pcbsytem@hotmail.com>
2020-01-30 07:19:17 -0300

    Include structure

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const servidor = http.createServer((req, res) => {
+
+  let html = '';
+  if (req.url === '/') {
+    html = `
+      <html>
+        <head>  
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1>Hello word</h1>
+        </body>
+      </html>        
+      `;
+  }
+  res.end(html);
+});
+
+servidor.listen(3000);
