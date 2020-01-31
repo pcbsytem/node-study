3eb74c8cb476696af70e018c833e976bc75011a9
pcbsytem <pcbsytem@hotmail.com>
2020-01-31 07:45:40 -0300

    Changes in structure

--- server.js
+++ server.js
@@ -1,52 +1,33 @@
-const express = require('express');
-const app = express();
-const http = require('http');
+const app = require('./src/config/custom-express');
 
 app.listen(3001, () => {
   console.log('Servidor roandando na porta 3000');
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
   res.send(`
     <html>
       <head>  
         <meta charset="utf-8">
       </head>
       <body>
-        <h1>Churi</h1>
+        <h1>Bem vindo!</h1>
       </body>
     </html> 
   `
-  );
-})
+  )
+)
 
-// const servidor = http.createServer((req, res) => {
-
-//   let html = '';
-//   if (req.url === '/') {
-//     html = `
-//       <html>
-//         <head>  
-//           <meta charset="utf-8">
-//         </head>
-//         <body>
-//           <h1>Home</h1>
-//         </body>
-//       </html>        
-//       `;
-//   } else if (req.url === '/livros') {
-//     html = `
-//       <html>
-//         <head>  
-//           <meta charset="utf-8">
-//         </head>
-//         <body>
-//           <h1>Livros</h1>
-//         </body>
-//       </html>        
-//     `;
-//   }
-//   res.end(html);
-// });
-
-// servidor.listen(3000);
+app.get('/livros', (req, res) =>
+  res.send(`
+    <html>
+      <head>  
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Livros</h1>
+      </body>
+    </html> 
+  `
+  )
+)
