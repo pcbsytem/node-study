5e3f2eb991b4ee4326ffe14cdb36a8b594c09e3f
pcbsytem <pcbsytem@hotmail.com>
2020-02-25 07:26:05 -0300

    Session config

--- src/config/sessao-autenticacao.js
+++ src/config/sessao-autenticacao.js
@@ -26,4 +26,29 @@ module.exports = (app) => {
         .catch(erro => done(erro, false));
     }
   ));
+
+  passport.serializeUser((usuario, done) => {
+    const usuarioSessao = {
+      nome: usuario.nome_completo,
+      email: usuario.email
+    };
+
+    done(null, usuarioSessao);
+  });
+
+  passport.deserializeUser((usuarioSessao, done) => {
+    done(null, usuarioSessao);
+  });
+
+  app.use(sessao({
+    secret: 'node study',
+    genid: (req) => {
+      return uuid();
+    },
+    resave: false,
+    saveUninitialized: false
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 }
